46c05909c3d5f72c7a968675e0bb7c4c640ce527
Tom Dietel <tom@dietel.net>
2021-02-04 23:53:48 +0200

    Add formula for trapcc

---
+++ trapcc.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Trapcc < Formula
+  desc ""
+  homepage ""
+  url "https://alicetrd.web.cern.ch/download/src/trapcc-v0.8.4.tar.gz"
+  sha256 "bced92f6b75b014577a0b8c2d0d9103945812b9c07f8b1e3a6d26a83485fb689"
+  license "GPL-3.0-or-later"
+
+  depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    #system "./configure", "--disable-debug",
+    #                      "--disable-dependency-tracking",
+    #                      "--disable-silent-rules",
+    #                      "--prefix=#{prefix}"
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test trapcc-v`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "true"
+  end
+end
